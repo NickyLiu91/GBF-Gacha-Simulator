92b32c68476858cd20318f2d7b645a3761288889
NickyLiu91 <nickyliu91@gmail.com>
2019-03-26 23:47:42 -0400

    added crystal page component and set up

---
+++ src/components/CrystalPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CrystalPage = (props) => {
+  return (
+    <p>"hi"</p>
+  )
+}
+
+export default CrystalPage;
--- src/containers/PickSummon.js
+++ src/containers/PickSummon.js
@@ -1,19 +1,35 @@
 import React from "react"
+import CrystalPage from '../components/CrystalPage'
 
 export default class PickSummon extends React.Component {
 
+  state = {
+    display: "PickSummon"
+  }
+
+
   render () {
-    return(
-      <div id="page">
-        <div>
-          <div id="ten-summon">
-            <p>Ten Summon</p>
-          </div>
-          <div id="single-summon">
-            <p>Single Summon</p>
+    if (this.state.display === "PickSummon") {
+      return(
+        <div id="page">
+          <div>
+            <div id="ten-summon">
+              <p>Ten Summon</p>
+              <img src="" />
+              <button> 3000 Crystals</button>
+            </div>
+            <div id="single-summon">
+              <p>Single Summon</p>
+              <img src="" />
+              <button> 300 Crystals</button>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return(
+        <CrystalPage />
+      )
+    }
   }
 }
