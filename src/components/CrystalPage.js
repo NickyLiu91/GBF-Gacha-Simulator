e9a31ba0156d001096bdd9d4f6c829f05d089aa2
NickyLiu91 <nickyliu91@gmail.com>
2019-06-25 15:56:58 -0400

    successfully added different color crystals based on what you rolled

--- src/components/CrystalPage.js
+++ src/components/CrystalPage.js
@@ -2,18 +2,71 @@ import React from "react";
 import {connect} from 'react-redux';
 
 const CrystalPage = (props) => {
-  return (
-    <div id="crystal-page">
-      <div id="summon-area">
-        <div id="crystal" onClick={event => {props.changeDisplay("WeaponPage")}}>
-          <div id="summon-crystal-top">
-          </div>
-            <div id="summon-crystal-bottom">
+  if (props.result.filter(object => object.rarity == "R").length == props.result.length) {
+    return (
+      <div id="crystal-page">
+        <div id="summon-area">
+          <div id="crystal" onClick={event => {props.changeDisplay("WeaponPage")}}>
+            <div id="summon-crystal-top-blue">
+            </div>
+              <div id="summon-crystal-bottom-blue">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  } else if (props.result.filter(object => object.rarity == "SSR").length > 0) {
+    let output = Math.floor(Math.random() * 100)
+    console.log(output)
+    if (output > 50) {
+      return (
+        <div id="crystal-page">
+          <div id="summon-area">
+            <div id="crystal" onClick={event => {props.changeDisplay("WeaponPage")}}>
+              <div id="summon-crystal-top-rainbow">
+              </div>
+                <div id="summon-crystal-bottom-rainbow">
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div id="crystal-page">
+          <div id="summon-area">
+            <div id="crystal" onClick={event => {props.changeDisplay("WeaponPage")}}>
+              <div id="summon-crystal-top-brown">
+              </div>
+                <div id="summon-crystal-bottom-brown">
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  } else {
+    let output = Math.floor(Math.random() * 100)
+    console.log(props.result.filter(object => object.rarity == "SSR"))
+    return (
+      <div id="crystal-page">
+        <div id="summon-area">
+          <div id="crystal" onClick={event => {props.changeDisplay("WeaponPage")}}>
+            <div id="summon-crystal-top-brown">
+            </div>
+              <div id="summon-crystal-bottom-brown">
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    result: state.resultChanger.result,
+  }
 }
 
 const mapDispatchToProps = dispatch => {
@@ -23,5 +76,6 @@ const mapDispatchToProps = dispatch => {
 }
 
 export default connect(
-  null, mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(CrystalPage);
