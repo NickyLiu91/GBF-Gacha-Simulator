fb6c00168e9c496e93bd810e2021d71da1b1f3f0
NickyLiu91 <nickyliu91@gmail.com>
2019-07-20 18:49:59 -0400

    now shows character name along with weapon

--- src/components/TrackerSingle.js
+++ src/components/TrackerSingle.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const TrackerSingle = (props) => {
-  return (
-    <div id="tracker-single">
-      {props.item.quantity} {props.item.weapon}
-    </div>
-  )
+  if (props.item.character != "None") {
+    return (
+      <div id="tracker-single">
+        {props.item.quantity} {props.item.weapon} {"(" + props.item.character + ")"}
+      </div>
+    )
+  } else {
+    return (
+      <div id="tracker-single">
+        {props.item.quantity} {props.item.weapon}
+      </div>
+    )
+  }
+
 }
 
 export default TrackerSingle;
