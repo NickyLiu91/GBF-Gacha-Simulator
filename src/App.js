df8d8761c472ffbdca78945f3189c320795bc864
NickyLiu91 <nickyliu91@gmail.com>
2020-02-07 09:25:12 -0500

    fixed app.js importing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './containers/Home';
+import Tracker from './containers/Tracker';
+import Summon from './containers/PickSummon';
+import Result from './containers/ResultPage';
+import Character from './components/CharacterPage';
+import Weapon from './components/WeaponPage';
+import Crystal from './components/CrystalPage';
 import {Route, Link, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
-    );
+      <div>
+      <Switch>
+        <Route exact path={`/`} component={Home} />
+        <Route path={`/summon`} component={Summon} />
+        <Route path={`/crystal`} component={Crystal} />
+        <Route path={`/character`} component={Character} />
+        <Route path={`/weapon`} component={Weapon} />
+        <Route path={`/result`} component={Result} />
+      </Switch>
+      </div>
+    )
   }
 }
 
